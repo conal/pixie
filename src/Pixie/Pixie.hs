7e422e2ecdf2098018846ec53d030ee6d7f7adbb
Conal Elliott <conal@conal.net>
2012-12-20 10:55:20 -0800

    code shuffle; comments; prim

--- src/Pixie/Pixie.hs
+++ src/Pixie/Pixie.hs
@@ -4,6 +4,9 @@
 -- {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -Wall #-}
 
+-- Experiment
+{-# LANGUAGE ImpredicativeTypes #-}
+
 -- For Transformable WriterArrow instance
 {-# LANGUAGE TypeFamilies, UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -56,37 +59,48 @@ runPixie q a = runWriter (runTSFun q) a
 -- | Diagrams containing paths (renderable wherever paths are).
 type Diag = forall e. Renderable (Path R2) e => QDiagram e R2 Any
 
+-- | Diagram writer
+type DWriter a b = 
+  forall e. Renderable (Path R2) e => WriterArrow (QDiagram e R2 Any) (->) a b
+
+-- Alternative. Requires ImpredicativeTypes. Is that forall type a Monoid?
+type DWriter' = 
+  WriterArrow (forall e. Renderable (Path R2) e => QDiagram e R2 Any) (->)
+
 -- | Circuit picture with typed inputs & outputs
 type a :> b = forall e. Renderable (Path R2) e => Pixie e R2 Any a b
+
+-- Alternative
+type (:>:) = TSFun (Point R2) DWriter'
 
 -- | Input or output port collection. Currently just a position per component.
 type Ports a = TS a P2
 
--- | Draw circuit, given input positions
-draw :: a :> b -> Ports a -> IO (Ports b)
-draw q a = defaultMain (d # pad 1.1) >> return b
- where
-   (b,d) = runPixie q a
-
 {--------------------------------------------------------------------
-    Belongs elsewhere (orphans)
+    Utilities
 --------------------------------------------------------------------}
 
--- Needed for Transformable instance
-type instance V (WriterArrow w (~>) a b) = V (a ~> (b,w))
+-- | Composition with transformation.
+comp :: (Category (~>), Transformable (b ~> c), V (b ~> c) ~ v) =>
+        Transformation v -> a ~> b -> b ~> c -> a ~> c
+comp xf a b = a >>> transform xf b
 
-instance (Arrow (~>), Monoid w, Transformable (a ~> (b,w))) =>
-         Transformable (WriterArrow w (~>) a b) where
-  transform tr = WriterArrow . transform tr  . runWriter
+-- | Composition with translation.
+compTranslate :: (Category (~>), Transformable (b ~> c), V (b ~> c) ~ v) =>
+                 v -> a ~> b -> b ~> c -> a ~> c
+compTranslate = comp . translation
 
---     Constraint is no smaller than the instance head
---       in the constraint: Transformable ((~>) a (b, w))
---     (Use -XUndecidableInstances to permit this)
---     In the instance declaration for `Transformable (WriterArrow w ~> a b)'
-
-{--------------------------------------------------------------------
-    Tests
---------------------------------------------------------------------}
+-- Some specialized operators. The right-associativity is critical here for
+-- cumulative transformation, since 'comp' transforms its right argument.
+infixr 3 -|-, -|/, -|\, -|*, -|&
+(-|-),(-|/),(-|\),(-|*),(-|&) :: 
+   (Category (~>), Transformable (b ~> c), V (b ~> c) ~ R2) =>
+   a ~> b -> b ~> c -> a ~> c
+(-|-) = compTranslate (r2 (1, 0))
+(-|/) = compTranslate (r2 (1, 1))
+(-|\) = compTranslate (r2 (1,-1))
+(-|*) = comp (translation (r2 (0.75,0)) <> scaling 0.6)
+(-|&) = comp (rotation (1/15 :: CircleFrac) <> translation (r2 (1,0)) <> scaling 0.7)
 
 port :: Colour Double -> P2 -> Diag
 port c p = circle 0.02 # fc c # translate (unPoint p)
@@ -115,22 +129,18 @@ src ~~> snk = seg <> h
        # translate (unPoint snk)
    angle v = Rad (uncurry (flip atan2) (unr2 v))
 
-(~*>) :: P2 -> P2 -> Diag
-src ~*> snk = src ~~> snk <> iPort snk
+-- | Generate output picture
+class OutDiag a where
+  outDiag :: a -> Diag
 
--- | @Bool -> Bool@ circuit
-type BC = Bool :> Bool
+instance OutDiag P2 where outDiag = oPort
+instance (OutDiag a, OutDiag b) => OutDiag (a,b) where
+  outDiag (a,b) = outDiag a <> outDiag b
 
--- 'BC' with input on the left and output on the right and given width and
--- height.
-bc :: Double -> Double -> BC
-bc w h = TF $
-  proc src -> do
-    let -- input & output ports
-        ip = p2 (-0.5*w,0)
-        op = p2 ( 0.5*w,0)
-    write -< freeze $ (src ~*> ip) <> oPort op <> rect w h
-    returnA -< op
+-- | Render and return output
+output :: OutDiag a => DWriter a a
+output = proc o -> do write   -< freeze (outDiag o)
+                      returnA -< o
 
 -- The 'freeze' causes line widths to scale under transformation. I like that it
 -- maintains visual balance within boxes & arrows. Doubting, though. And where
@@ -151,63 +161,94 @@ bc w h = TF $
 -- compose it with a circuit somewhere else, I'll get an arrow drawn, which
 -- breaks identity laws.
 
+-- | Generate input picture from source and sink, including arrow.
+class InDiag a where
+  (~*>) :: a -> a -> Diag
+
+instance InDiag P2 where
+  src ~*> snk = src ~~> snk <> iPort snk
+
+instance (InDiag a, InDiag b) => InDiag (a,b) where
+  (a,b) ~*> (a',b') = (a ~*> a') <> (b ~*> b')
+
+prim :: (InDiag (Ports a), OutDiag (Ports b)) =>
+        Diag -> Ports a -> Ports b -> a :> b
+prim d a b = TF $
+  proc src -> do
+    write  -< freeze (d <> (src ~*> a))
+    output -< b
+
+
+
+{--------------------------------------------------------------------
+    Belongs elsewhere (orphans)
+--------------------------------------------------------------------}
+
+-- Needed for Transformable instance
+type instance V (WriterArrow w (~>) a b) = V (a ~> (b,w))
+
+instance (Arrow (~>), Monoid w, Transformable (a ~> (b,w))) =>
+         Transformable (WriterArrow w (~>) a b) where
+  transform tr = WriterArrow . transform tr  . runWriter
+
+--     Constraint is no smaller than the instance head
+--       in the constraint: Transformable ((~>) a (b, w))
+--     (Use -XUndecidableInstances to permit this)
+--     In the instance declaration for `Transformable (WriterArrow w ~> a b)'
+
+{--------------------------------------------------------------------
+    Tests
+--------------------------------------------------------------------}
+
+-- | Draw circuit, given input positions
+draw :: a :> b -> Ports a -> IO (Ports b)
+draw q a = defaultMain (d # pad 1.1) >> return b
+ where
+   (b,d) = runPixie q a
+
+-- | @Bool -> Bool@ circuit
+type BB = Bool :> Bool
+
+-- Draw with input from (-0.5,0)
+drawBB :: BB -> IO P2
+drawBB c = draw c (p2 (-0.5,0))
+
+-- 'BB' with input on the left and output on the right and given width and
+-- height.
+bb :: Double -> Double -> BB
+bb w h = prim (rect w h) (p2 (-w/2,0)) (p2 (w/2,0))
+
 t4 :: IO P2
-t4 = draw (bc 0.5 1) (p2 (-0.75,0.5))
+t4 = draw (bb 0.5 1) (p2 (-0.75,0.5))
 
 t5from :: R2 -> IO P2
-t5from v = draw (translate v (bc 0.5 1)) (p2 (-0.75,0.5))
+t5from v = draw (translate v (bb 0.5 1)) (p2 (-0.75,0.5))
 
 t5a, t5b :: IO P2
 t5a = t5from (r2 ( 0.5,0))
 t5b = t5from (r2 (-1.0,0))
 
--- | Composition with transformation.
-comp :: (Category (~>), Transformable (b ~> c), V (b ~> c) ~ v) =>
-        Transformation v -> a ~> b -> b ~> c -> a ~> c
-comp xf a b = a >>> transform xf b
-
--- | Composition with translation.
-compTranslate :: (Category (~>), Transformable (b ~> c), V (b ~> c) ~ v) =>
-                 v -> a ~> b -> b ~> c -> a ~> c
-compTranslate = comp . translation
-
--- Some specialized operators. The right-associativity is critical here for
--- cumulative transformation, since 'comp' transforms its right argument.
-infixr 3 -|-, -|/, -|\, -|*, -|&
-(-|-),(-|/),(-|\),(-|*),(-|&) :: 
-   (Category (~>), Transformable (b ~> c), V (b ~> c) ~ R2) =>
-   a ~> b -> b ~> c -> a ~> c
-(-|-) = compTranslate (r2 (1, 0))
-(-|/) = compTranslate (r2 (1, 1))
-(-|\) = compTranslate (r2 (1,-1))
-(-|*) = comp (translation (r2 (0.75,0)) <> scaling 0.6)
-(-|&) = comp (rotation (1/15 :: CircleFrac) <> translation (r2 (1,0)) <> scaling 0.7)
-
--- Draw with input from (-0.5,0)
-drawBB :: TS a P2 ~ P2 => (a :> b) -> IO (TS b P2)
-drawBB c = draw c (p2 (-0.5,0))
-
 -- Use drawBB with the following examples.
 
-bc1, bc2 :: BC
-bc1 = bc 0.5 0.75
-bc2 = bc 0.75 0.5
+bb1, bb2 :: BB
+bb1 = bb 0.5 0.75
+bb2 = bb 0.75 0.5
 
-bcs1 :: BC
-bcs1 = bc1 -|- bc2 -|- bc1 -|- bc2 -|- bc1
+bbs1 :: BB
+bbs1 = bb1 -|- bb2 -|- bb1 -|- bb2 -|- bb1
 
-bcs2 :: BC
-bcs2 = bc1 -|/ bc2 -|\ bc1 -|\ bc2 -|/ bc1 -|- bc2
+bbs2 :: BB
+bbs2 = bb1 -|/ bb2 -|\ bb1 -|\ bb2 -|/ bb1 -|- bb2
 
-bcs3 :: BC
-bcs3 = b -|* b -|* b -|* b -|* b -|* b
+bbs3 :: BB
+bbs3 = b -|* b -|* b -|* b -|* b -|* b
  where
-   b = bc 0.75 0.75
+   b = bb 0.75 0.75
 
-bcs4 :: BC
-bcs4 = b -|& b -|& b -|& b -|& b -|& b
+bbs4 :: BB
+bbs4 = b -|& b -|& b -|& b -|& b -|& b
  where
-   b = bc 0.75 0.75
+   b = bb 0.75 0.75
 
 {--------------------------------------------------------------------
     One-bit full adder
@@ -216,43 +257,9 @@ bcs4 = b -|& b -|& b -|& b -|& b -|& b
 -- | Take a a carry-in from the left and pair of addend bits from above, and
 -- yield a sum bit below and a carry-out bit on the right.
 addB :: (Bool,(Bool,Bool)) :> (Bool,Bool)
-addB = TF $
-  proc (i,(a,b)) -> do
-    let -- input & output ports
-        ip = p2 (-1/2, 0  )
-        ap = p2 (-1/6, 1/2)
-        bp = p2 ( 1/6, 1/2)
-        sp = p2 (   0,-1/2)
-        op = p2 ( 1/2, 0  )
-    write -< freeze $   (i ~*> ip) <> (a ~*> ap) <> (b ~*> bp)
-                     <> oPort sp <> oPort op
-                     <> unitSquare
-    returnA -< (sp,op)
-
--- TODO: Refactor with convenience functions:
---
--- *   Combine (~~>) and iPort into (~*>). Done.
--- *   Combine returing of output ports and generating their pictures.
+addB = prim unitSquare
+         (p2 (-1/2, 0), (p2 (-1/6, 1/2), p2 ( 1/6, 1/2)))
+         (p2 (0,-1/2), p2 (1/2, 0))
 
 drawAddB :: (Bool,(Bool,Bool)) :> (Bool,Bool) -> IO (P2,P2)
 drawAddB c = draw c (p2 (-1,0),(p2 (-1/6,1), p2 ( 1/6,1)))
-
-
-class OutPic a where
-  outPic :: a -> Diag
-
-instance OutPic P2 where outPic = oPort
-instance (OutPic a, OutPic b) => OutPic (a,b) where
-  outPic (a,b) = outPic a <> outPic b
-
-outs :: (Renderable (Path R2) e, OutPic a) =>
-        WriterArrow (QDiagram e R2 Any) (->) a a
-outs = proc o -> do write -< freeze (outPic o)
-                    returnA -< o
-
--- type Pixie e v m = TSFun (Point v) (WriterArrow (QDiagram e v m) (->))
-
--- type Diag = forall e. Renderable (Path R2) e => QDiagram e R2 Any
-
--- -- | Circuit picture with typed inputs & outputs
--- type a :> b = forall e. Renderable (Path R2) e => Pixie e R2 Any a b
