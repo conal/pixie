5cb7edbfd8e92b556a8565b47218093daf448ba8
Conal Elliott <conal@conal.net>
2013-01-04 18:21:23 -0800

    Tidying

--- src/Pixie/Pixie.hs
+++ src/Pixie/Pixie.hs
@@ -13,10 +13,10 @@
 {-# LANGUAGE ImpredicativeTypes #-}
 
 -- For Transformable WriterArrow instance
-{-# LANGUAGE TypeFamilies, UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE TypeFamilies, UndecidableInstances #-}
 
-{-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
+-- {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
 -- {-# OPTIONS_GHC -fno-warn-unused-binds   #-} -- TEMP
 
 ----------------------------------------------------------------------
@@ -46,6 +46,8 @@ import Control.Arrow.Transformer.Writer
 -- import Control.Arrow.Transformer.State
 import System.Process (system)
 
+import Control.Compose ((<~)) -- TypeCompose
+
 import TypeUnary.Vec
 
 import Data.AffineSpace.Point
@@ -65,25 +67,24 @@ import Pixie.TSFunTF -- or TSFunGadt
 
 -- For now, but remove the WriterArrow orphan later
 #include "CatSynth/Has-inc.hs"
+
 #include "CatSynth/Decode-inc.hs"
 
--- type Pins v a = TS a (Point v)
---
--- type Pixie' e v m a b = Pins v a -> (Pins b v, QDiagram e v m)
+-- TODO: Move e to after v & m for convenient partial application.
+type PTS v c = TS c (Point v)
+
+-- type Pixie' e v m a b = PTS v (Decoding a) -> (PTS v (Decoding b), QDiagram e v m)
 
 -- | Circuit picture arrow in its most general form. See also 'Pixie'.
 type Pixie e v m = Strip (TSFun (Point v) (WriterArrow (QDiagram e v m) (->)))
 
--- TODO: Move e to after v & m for convenient partial application.
-
-type DecodeP v c = ( Decode (TS c (Point v))
-                   , Decoding (TS c (Point v)) ~ TS (Decoding c) (Point v) )
+type DecodeP v c = (Decode (PTS v c), Decoding (PTS v c) ~ PTS v (Decoding c))
 type DecodePP v a b = (DecodeP v a, DecodeP v b)
 
 runPixie :: ( Semigroup m, Floating (Scalar v), Ord (Scalar v)
             , InnerSpace v, HasLinearMap v ) =>
             DecodePP v a b =>
-            Pixie e v m a b -> TS a (Point v) -> (TS b (Point v), QDiagram e v m)
+            Pixie e v m a b -> PTS v a -> (PTS v b, QDiagram e v m)
 runPixie q a = first encode $ (runWriter.runTSFun.unA) q (decode a)
 
 -- | Diagrams containing paths (renderable wherever paths are).
@@ -98,24 +99,19 @@ type Diag = Diag' R2 Any
 type DWriter a b = 
   forall e. Renderable (Path R2) e => WriterArrow (QDiagram e R2 Any) (->) a b
 
--- -- Alternative. Requires ImpredicativeTypes. Is that forall type a Monoid?
--- type DWriter' = 
---   WriterArrow (forall e. Renderable (Path R2) e => QDiagram e R2 Any) (->)
-
 -- | Circuit picture with typed inputs & outputs
 type a :> b = forall e. Renderable (Path R2) e => Pixie e R2 Any a b
-
--- Inside a '(:>)'
-type a :>: b = DWriter (Decoding (Ports a)) (Decoding (Ports b))
-
--- -- Alternative
--- type (:>:) = TSFun P2 DWriter'
 
 -- | Input or output port collection. Currently just a position per component.
 type Ports a = TS a P2
 
 -- | Ports of decoding
 type DPorts a = Ports (Decoding a)
+
+-- -- Inside a '(:>)'
+-- type a :>: b = DWriter (DPorts a) (DPorts b)
+
+-- type a :>: b = DWriter (Decoding (Ports a)) (Decoding (Ports b))
 
 {--------------------------------------------------------------------
     Utilities
@@ -150,9 +146,6 @@ iPort, oPort :: P2 -> Diag
 iPort = port white
 oPort = port black
 
--- p2 :: (Double,Double) -> P2
--- p2 = P . r2
-
 -- | Arrow from source to sink
 (~~>) :: P2 -> P2 -> Diag
 src ~~> snk = seg <> h
@@ -171,8 +164,7 @@ src ~~> snk = seg <> h
    angle v = Rad (uncurry (flip atan2) (unr2 v))
 
 -- | Generate output picture
-class OutDiag a where
-  outDiag :: a -> Diag
+class OutDiag a where outDiag :: a -> Diag
 
 instance OutDiag P2 where outDiag = oPort
 instance (OutDiag a, OutDiag b) => OutDiag (a,b) where
@@ -203,8 +195,7 @@ output = proc o -> do write   -< freeze (outDiag o)
 -- breaks identity laws.
 
 -- | Generate input picture from source and sink, including arrow.
-class InDiag a where
-  (~*>) :: a -> a -> Diag
+class InDiag a where (~*>) :: a -> a -> Diag
 
 instance InDiag P2 where
   src ~*> snk = src ~~> snk <> iPort snk
@@ -212,14 +203,10 @@ instance InDiag P2 where
 instance (InDiag a, InDiag b) => InDiag (a,b) where
   (a,b) ~*> (a',b') = (a ~*> a') <> (b ~*> b')
 
--- (~+>) :: (AdditiveGroup a, InDiag a) => a -> a -> Diag
--- src ~+> snk = src ~*> (src ^+^ snk)
-
 type Diags a b = (DecodePP R2 a b, InDiag (DPorts a), OutDiag (DPorts b))
 
 -- | Generalized 'prim' with ports computed from source.
-primF :: Diags a b =>
-         (Ports a -> (Diag, Ports a, Ports b)) -> a :> b
+primF :: Diags a b => (Ports a -> (Diag, Ports a, Ports b)) -> a :> b
 primF f = A $ TF $
   proc src -> do
     let (d,a,b) = f (encode src)
@@ -227,17 +214,19 @@ primF f = A $ TF $
     output -< decode b
 
 -- | Primitive circuit component with inputs & outputs
-prim :: Diags a b =>
-        Diag -> Ports a -> Ports b -> a :> b
+prim :: Diags a b => Diag -> Ports a -> Ports b -> a :> b
 prim d a b = primF (const (d,a,b))
 
-type Transfo c = (Transformable (Ports c), V (Ports c) ~ R2)
-type Transfos a b = (Transfo a, Transfo b)
+-- Some constraint shorthands
+type Transfo   x   = (Transformable x, V x ~ R2)
+type PTransfo  c   = Transfo (Ports c)
+type PTransfos a b = (PTransfo a, PTransfo b)
 
-primDel :: forall a b. (Diags a b, Transfos a b) =>
+-- | 'prim' with delta computed from input ports
+primDel :: forall a b. (Diags a b, PTransfos a b) =>
            (Ports a -> R2) -> Diag -> Ports a -> Ports b -> a :> b
 primDel del diag a b =
-  primF (\ w -> let f :: forall t. (Transformable t, V t ~ R2) => Unop t
+  primF (\ w -> let f :: forall t. Transfo t => Unop t
                     f = translate (del w) in 
                   (f diag, f a, f b))
  
@@ -250,9 +239,7 @@ type instance V (WriterArrow w (~>) a b) = V (a ~> (b,w))
 
 instance (Arrow (~>), Monoid w, Transformable (a ~> (b,w))) =>
          Transformable (WriterArrow w (~>) a b) where
-  transform tr = WriterArrow . transform tr  . runWriter
-  -- SEC-style:
-  -- transform = inWriter . transform
+  transform = inWriter . transform
 
 --     Constraint is no smaller than the instance head
 --       in the constraint: Transformable ((~>) a (b, w))
@@ -262,47 +249,39 @@ instance (Arrow (~>), Monoid w, Transformable (a ~> (b,w))) =>
 type instance V (Vec n t) = V t
 
 instance Transformable t => Transformable (Vec n t) where
-  transform xf = fmap (transform xf)
-  -- transform = fmap . transform
+  transform = fmap . transform
 
 type instance V (Strip (~>) a b) = V (StripX (~>) a b)
 
 instance (HasLinearMap (V (StripX (~>) a b)), Transformable (StripX (~>) a b))
       => Transformable (Strip (~>) a b) where
-  transform tr = inA (transform tr)
-  -- transform = inA . transform
-
--- type instance Decoding (Point v) = Decoding v
--- instance Decode v => Decode (Point v) where
---   decode = decode . unPoint
---   encode = Point . encode
-
--- type instance Decoding R2 = (Double,Double)
--- instance Decode R2 where
---   encode = r2
---   decode = unr2
-
--- type instance Decoding (Point v) = Point v
--- instance Category ar => Decode ar (Point v) where
---   decode = id
---   encode = id
+  transform = inA . transform
 
 DecodePrim(Point v)
 
 TransInstances(lift,WriterArrow w,(Arrow (~>), Monoid w))
+
+-- newtype WriterArrow w a b c = WriterArrow (a b (c, w))
+
+inWriter :: (Arrow a, Monoid w) =>
+            (a b (c, w) -> a' b' (c', w'))
+         -> (WriterArrow w a b c -> WriterArrow w' a' b' c')
+inWriter = WriterArrow <~ runWriter
 
 
 {--------------------------------------------------------------------
     Tests
 --------------------------------------------------------------------}
 
+-- | Output a diagram in SVG, and convert to PDF. Assumes ImageMagick is
+-- installed, including the @convert@ program.
+out :: Diagram SVG R2 -> IO ()
+out d = do defaultMain (d # pad 1.1)
+           void (system "convert output.svg output.pdf")
+
 -- | Draw circuit, given input positions
-draw :: DecodePP R2 a b =>
-        Ports a -> a :> b -> IO ()
-draw a q = do defaultMain (d # pad 1.1)
-              void (system "convert output.svg output.pdf")
- where
-   (_,d) = runPixie q a
+draw :: DecodePP R2 a b => Ports a -> a :> b -> IO ()
+draw a q = out (snd (runPixie q a))
 
 -- | @Bool -> Bool@ circuit
 type BB = Bool :> Bool
@@ -361,9 +340,6 @@ addB = prim unitSquare
 
 drawAddB :: ((Bool,Bool),Bool) :> (Bool,Bool) -> IO ()
 drawAddB = draw ((p2 (-1/6,1), p2 (1/6,1)), p2 (-1,0))
-
--- primDel :: forall a b. (Diags a b, Transfos a b) =>
---            (Ports a -> R2) -> Diag -> Ports a -> Ports b -> a :> b
 
 addBDel :: ((Bool,Bool),Bool) :> (Bool,Bool)
 addBDel = primDel
@@ -457,7 +433,9 @@ runPixieSt :: ( Semigroup m, Floating (Scalar v), Ord (Scalar v)
               , InnerSpace v, HasLinearMap v ) =>
               (DecodePP v a b, DecodeP v s) =>
               PixieSt e v m s a b -> TS (a,s) (Point v) -> (TS (b,s) (Point v), QDiagram e v m)
-runPixieSt (StateArrow q) a = runPixie q a
+runPixieSt = runPixie . runState
+
+-- runPixieSt (StateArrow q) a = runPixie q a
 
 type (:#>) = PixieSt SVG R2 Any Bool
 
@@ -501,8 +479,7 @@ psDiag a q = snd (runPixieSt q a)
 
 drawAddSV :: (IsNat n, a ~ Vec n (Bool, Bool), DecodePP R2 a b) =>
              (a :#> b) -> IO ()
-drawAddSV q = defaultMain (psDiag addVIns q # pad 1.1)
+drawAddSV = out . psDiag addVIns
 
 t :: IO ()
 t = drawAddSV (addA :: AddSV N8)
-
